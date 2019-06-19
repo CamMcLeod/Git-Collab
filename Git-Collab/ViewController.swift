7649eb0a98bdb7ccad3b72d15612dc8cc6ad5e2d
Cam McLeod <cammcleod3@gmail.com>
2019-06-18 18:44:19 -0700

    added color and modelmanager struct

--- Git-Collab/ViewController.swift
+++ Git-Collab/ViewController.swift
@@ -13,6 +13,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        struct Color {
+            var color: UIColor
+            var description: String
+            
+            init(withColor: UIColor) {
+                self.color = withColor
+                self.description = "The color is \(self.color)"
+            }
+            
+        }
+        
+        struct ModelManager {
+            var colors: [Color] = []
+            
+            init() {
+                for i in 1...10 {
+                    let mult : CGFloat = CGFloat(integerLiteral: i)
+                    self.colors.append(Color(withColor: UIColor.init(displayP3Red:mult/10, green: mult/10, blue: mult/10, alpha: 1)))
+                }
+            }
+            
+        }
     }
 
 
